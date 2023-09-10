96803f9a92a58ea02b640dc9e342a59515552425
kimgalina <genrietta.kim@gmail.com>
2023-09-10 19:34:45 +0600

    final version of solution

--- src/Main.java
+++ src/Main.java
@@ -13,11 +13,25 @@ public class Main {
         // классы BufferedReader , PrintWriter предоставляют возможность строкового ввода вывода
        try( BufferedReader in = new BufferedReader( new FileReader("input.txt"));
             PrintWriter out = new PrintWriter(new FileWriter("output.txt"))){
+           // читаем первую строку
             String str = in.readLine();
-            int sum1 = 0;
-            sum1+=Integer.parseInt(str);
-            
-
+            int sum1 = 0, sum2=0;
+            int firstNum = Integer.parseInt(str);
+            sum1+=firstNum;
+           // читаем вторую строку
+           str = in.readLine();
+           String[] numbersStr = str.split(" ");
+          for(int i = 0; i < numbersStr.length - 1;i++){
+              sum1+= Integer.parseInt(numbersStr[i]);
+          }
+          // читаем третью строку
+           str = in.readLine();
+           numbersStr = str.split(" ");
+           for(int i = 0; i < numbersStr.length ;i++){
+               sum2+= Integer.parseInt(numbersStr[i]);
+           }
+           out.println(sum1);
+           out.println(sum2);
        }catch(FileNotFoundException exc){
            System.err.println("Sorry but the file wasnt found");
        }catch(IOException exc){
